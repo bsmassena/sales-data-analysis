5d1ccc7e1d208021b705efeccfb0bec4c63f9967
Bruno Santana <bsmassena@gmail.com>
2020-05-12 22:14:43 -0300

    refactor: improve directory watcher code

--- src/main/java/com/bsmlima/salesdataanalysis/DirectoryWatcher.java
+++ src/main/java/com/bsmlima/salesdataanalysis/DirectoryWatcher.java
@@ -1,25 +1,29 @@
 package com.bsmlima.salesdataanalysis;
 
+import com.bsmlima.salesdataanalysis.dao.ReportDao;
+import com.bsmlima.salesdataanalysis.parser.FileParser;
+
 import java.io.IOException;
 import java.nio.file.*;
 
 public class DirectoryWatcher {
 
-    private WatchService watchService;
+    private final WatchService watchService;
+    private FileParser fileParser;
 
-    public DirectoryWatcher() throws IOException {
-        watchService = FileSystems.getDefault().newWatchService();
+    public DirectoryWatcher(FileParser fileParser, ReportDao reportDao) throws IOException {
+        this.fileParser = fileParser;
+        this.watchService = FileSystems.getDefault().newWatchService();
 
-        Path path = Paths.get(System.getenv("HOME") + "/data/in");
-        WatchKey key = path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+        Path path = Paths.get(reportDao.getInputPath());
+        path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
     }
 
-    public void watch() throws InterruptedException {
+    public void watch() throws InterruptedException, IOException {
         while (true) {
             WatchKey queuedKey = watchService.take();
             for (WatchEvent<?> watchEvent : queuedKey.pollEvents()) {
-                System.out.printf("Created: %s\n", watchEvent.context());
-
+                fileParser.parse(watchEvent.context().toString()); // TODO: catch errors on parsing
                 queuedKey.reset();
             }
         }
